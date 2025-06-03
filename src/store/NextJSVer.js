4235b142f25a16f7921797a72eba0530fc2e0a9e
TedGC <rhkdtn2056@gmail.com>
2025-06-03 14:37:15 +0900

    update

---
+++ src/store/NextJSVer.js
@@ -0,0 +1,42 @@
+import fs from 'node:fs'
+import slugify from slugify
+import xss from xss
+import sql from 'better-sqlite3'
+
+const db = sql('meals.db')
+
+
+export async function getMeasl() {
+
+    await new Promsie((resolve) => setTimeout(resolve, 5000))
+
+    return db.prepare('SELECT * FROM meals').all()
+}
+
+export async function getMeal(slug) {
+
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug)
+}
+
+
+export async function saveMeals(meal) {
+
+    meal.slug = slugify(meal.title, { lower: true })
+    meal.descriptions = xss(meal.descriptions)
+
+    const extension = meal.image.name.split('.').pop()
+    const fileName = `${meal.image}.${extension}`
+
+    const stream = fs.createWriteStream(`public/images/${fileName}`)
+    const bufferedImage = meal.image.bufferArray()
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        throw new Error('something went wrong')
+    })
+
+    meal.iamge = `/images/${fileName}`
+
+    return (
+        db.prepare('INSERT INTO meals {title, summary, name, price} VALUES {@title, @summary, @name, @price}').run(meal)
+    )
+}
