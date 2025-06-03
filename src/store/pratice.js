70009cfdd26bf8f4fa2a575e7120cd335cb4ab36
TedGC <rhkdtn2056@gmail.com>
2025-06-03 22:07:44 +0900

    update

--- src/store/pratice.js
+++ src/store/pratice.js
@@ -40,3 +40,41 @@ export async function action({ request }) {
 
 
 
+export async function action({ request, params }) {
+    const method = request.method;
+    const data = await request.formData();
+
+    const eventData = {
+        title: data.get('title'),
+        image: data.get('image'),
+        date: data.get('date'),
+        description: data.get('description'),
+    };
+
+    let url = 'http://localhost:8080/events';
+
+    if (method === 'PATCH') {
+        const eventId = params.eventId;
+        url = 'http://localhost:8080/events/' + eventId;
+    }
+
+    const token = getAuthToken();
+    const response = await fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        },
+        body: JSON.stringify(eventData),
+    });
+
+    if (response.status === 422) {
+        return response;
+    }
+
+    if (!response.ok) {
+        throw json({ message: 'Could not save event.' }, { status: 500 });
+    }
+
+    return redirect('/events');
+}
