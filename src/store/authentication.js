5bdc5e531c6a9da4cc95b24328b5753b81e69372
TedGC <rhkdtn2056@gmail.com>
2025-06-03 17:39:30 +0900

    udpate

---
+++ src/store/authentication.js
@@ -0,0 +1,47 @@
+const { sign, verify } = require('jsonwebtoken');
+const { compare } = require('bcryptjs');
+const { NotAuthError } = require('./errors');
+
+
+
+const key = 'supersecret'
+
+function createJWTToken(email) {
+    return sign({ email }, key, { expiresIn: '1h' })
+}
+
+function validateJWTToken(token) {
+    return verify(token, key)
+}
+
+function comparePassword(password, sotredPassword) {
+    return compare(password, sotredPassword)
+}
+
+function checkAuthMiddleWare(req, res, next) {
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
+
+    if (!req.headers.authorization) {
+        return next(new NotAuthError('not found'))
+    }
+
+    const authFragments = red.headers.authorization.split('.')
+
+    if (authFragments !== 2) {
+        return next(new NotAuthError('not found'))
+    }
+
+    const token = authFragments[1]
+
+    try {
+        const validatedToken = validateJWTToken(authFragments)
+        req.token = validateJWTToken
+    }
+    catch (error) {
+        return next(new NotAuthError('not found '))
+    }
+
+    next()
+}
