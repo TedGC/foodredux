4cf35cc55426b0622c9d62d6128db37c2d0df830
TedGC <rhkdtn2056@gmail.com>
2025-06-04 10:37:58 +0900

    update

--- src/store/authentication.js
+++ src/store/authentication.js
@@ -2,46 +2,41 @@ const { sign, verify } = require('jsonwebtoken');
 const { compare } = require('bcryptjs');
 const { NotAuthError } = require('./errors');
 
+const KEY = 'supersecret';
 
-
-const key = 'supersecret'
-
-function createJWTToken(email) {
-    return sign({ email }, key, { expiresIn: '1h' })
+function createJSONToken(email) {
+    return sign({ email }, KEY, { expiresIn: '1h' });
 }
 
-function validateJWTToken(token) {
-    return verify(token, key)
+function validateJSONToken(token) {
+    return verify(token, KEY);
 }
 
-function comparePassword(password, sotredPassword) {
-    return compare(password, sotredPassword)
+function isValidPassword(password, storedPassword) {
+    return compare(password, storedPassword);
 }
 
-function checkAuthMiddleWare(req, res, next) {
+function checkAuthMiddleware(req, res, next) {
     if (req.method === 'OPTIONS') {
         return next();
     }
-
     if (!req.headers.authorization) {
-        return next(new NotAuthError('not found'))
+        console.log('NOT AUTH. AUTH HEADER MISSING.');
+        return next(new NotAuthError('Not authenticated.'));
     }
+    const authFragments = req.headers.authorization.split(' ');
 
-    const authFragments = req.headers.authorization.split('.')
-
-    if (authFragments !== 2) {
-        return next(new NotAuthError('not found'))
+    if (authFragments.length !== 2) {
+        console.log('NOT AUTH. AUTH HEADER INVALID.');
+        return next(new NotAuthError('Not authenticated.'));
     }
-
-    const token = authFragments[1]
-
+    const authToken = authFragments[1];
     try {
-        const validatedToken = validateJWTToken(authFragments)
-        req.token = validateJWTToken
+        const validatedToken = validateJSONToken(authToken);
+        req.token = validatedToken;
+    } catch (error) {
+        console.log('NOT AUTH. TOKEN INVALID.');
+        return next(new NotAuthError('Not authenticated.'));
     }
-    catch (error) {
-        return next(new NotAuthError('not found '))
-    }
-
-    next()
+    next();
 }
