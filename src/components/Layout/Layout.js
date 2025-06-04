f237475a612ffb924889b70e471f8c3a743cf031
TedGC <rhkdtn2056@gmail.com>
2025-06-04 23:10:36 +0900

    update

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -55,3 +55,23 @@ export async function shareMeal(prevState, formData) {
     creator: formData.get('name'),
     creator_email: formData.get('email'),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: 'Invalid input.',
+    };
+  }
+
+  await saveMeal(meal);
+  revalidatePath('/meals');
+  redirect('/meals');
+}
