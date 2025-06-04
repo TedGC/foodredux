032f587db28fd120413b63599f1a451d09b2bb86
TedGC <rhkdtn2056@gmail.com>
2025-06-04 23:10:26 +0900

    udpate

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -34,3 +34,24 @@ export async function action({ request }) {
   console.log(email);
   return { message: 'Signup successful!' };
 }
+
+'use server';
+
+import { redirect } from 'next/navigation';
+
+import { saveMeal } from './meals';
+import { revalidatePath } from 'next/cache';
+
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
+export async function shareMeal(prevState, formData) {
+  const meal = {
+    title: formData.get('title'),
+    summary: formData.get('summary'),
+    instructions: formData.get('instructions'),
+    image: formData.get('image'),
+    creator: formData.get('name'),
+    creator_email: formData.get('email'),
+  };
