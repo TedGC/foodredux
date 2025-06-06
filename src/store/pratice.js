1b0121bc6f6ddb0e8c604fea4b8a3b875e1ad7f4
TedGC <rhkdtn2056@gmail.com>
2025-06-06 22:07:00 +0900

    update

--- src/store/pratice.js
+++ src/store/pratice.js
@@ -101,3 +101,13 @@ function flattenObject(obj, prefix = '', res = {}) {
     }
     return res;
 }
+
+async function retry(fn, retries = 3, delay = 1000) {
+    try {
+        return await fn();
+    } catch (err) {
+        if (retries === 0) throw err;
+        await new Promise(res => setTimeout(res, delay));
+        return retry(fn, retries - 1, delay);
+    }
+}
