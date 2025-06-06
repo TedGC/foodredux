0769b2040a8e372e974859009a0957a3226f49ac
TedGC <rhkdtn2056@gmail.com>
2025-06-06 22:07:46 +0900

    update

--- src/store/pratice.js
+++ src/store/pratice.js
@@ -150,3 +150,13 @@ function groupBy(array, key) {
         return acc;
     }, {});
 }
+
+function debounceAsync(fn, delay) {
+    let timer;
+    return function (...args) {
+        clearTimeout(timer);
+        return new Promise(resolve => {
+            timer = setTimeout(() => resolve(fn(...args)), delay);
+        });
+    };
+}
